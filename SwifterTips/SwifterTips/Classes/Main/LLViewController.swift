2e65008a0c9f1fdd422562286ec25d40bd72cdcb
licheng <licheng0508@sina.com>
2018-09-27 14:31:42 +0800

    commit

---
+++ SwifterTips/SwifterTips/Classes/Main/LLViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LLViewController.swift
+//  SwifterTips
+//
+//  Created by licheng on 2018/9/27.
+//  Copyright © 2018年 licheng. All rights reserved.
+//
+
+import UIKit
+
+class LLViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
