dc858542c31be47e4e63b6f1316c2604df10dd02
licheng0508 <lc900508@sina.com>
2018-09-29 15:32:57 +0800

    commit

---
+++ SwifterTips/SwifterTips/Classes/Main/Tips/TipFour.swift
@@ -0,0 +1,18 @@
+//
+//  TipFour.swift
+//  SwifterTips
+//
+//  Created by licheng on 2018/9/29.
+//  Copyright © 2018 licheng. All rights reserved.
+//
+
+// MARK: - func 的参数修饰/typealias
+
+import UIKit
+
+func incrementor(variable: Int) -> Int {
+    return variable + 1
+}
+
+
+
